d55e58ee08b72a858b0cbdc095da73d3c7acf875
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-02-26 18:49:36 -0800

    added drop loot tables dependency
    * added AbstractAbilityDropper, to be used for combat and periodic ability-based drops
    * migrated previous event listener logic to CombatAbilityDropper

---
+++ src/main/kotlin/us/timinc/mc/cobblemon/fieldmoves/droppers/AbstractAbilityDropper.kt
@@ -0,0 +1,21 @@
+package us.timinc.mc.cobblemon.fieldmoves.droppers
+
+import com.cobblemon.mod.common.api.abilities.Ability
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.storage.loot.LootParams
+import us.timinc.mc.cobblemon.droploottables.api.droppers.AbstractDropper
+import us.timinc.mc.cobblemon.fieldmoves.CobblemonFieldMoves.modIdentifier
+import us.timinc.mc.cobblemon.fieldmoves.droppers.contexts.AbilityDropContext
+
+abstract class AbstractAbilityDropper(dropType: String) : AbstractDropper<AbilityDropContext>(dropType) {
+    open fun getAbilityDropId(ability: Ability) =
+        modIdentifier("$dropType/${ability.name}")
+
+    override fun getDrops(params: LootParams, context: AbilityDropContext): List<ItemStack> {
+        val results: MutableList<ItemStack> = mutableListOf()
+
+        results.addAll(getDropsFromTable(params.level, getAbilityDropId(context.ability), params))
+
+        return results
+    }
+}
